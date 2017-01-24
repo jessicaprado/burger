5ab44f4a8a9262d424e3f7b56251cd3c13c9bb8a
Jessica Prado <jessica.marie.prado@gmail.com>
2017-01-24 12:57:06 -0500

    starting out

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers_db.burgers (burger_name) VALUES ('Lettuce Wrapped Burger');
+INSERT INTO burgers_db.burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers_db.burgers (burger_name) VALUES ('Veggie Burger');
